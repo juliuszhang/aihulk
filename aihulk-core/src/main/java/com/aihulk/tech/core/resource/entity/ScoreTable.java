8b876939ae8ee896fd24416d852ec6f4e7e6d80a
zhangyibo <yibozhang4@creditease.cn>
2019-07-29 14:25:00 +0800

    增加评分卡类型的资源

---
+++ aihulk-core/src/main/java/com/aihulk/tech/core/resource/entity/ScoreTable.java
@@ -0,0 +1,12 @@
+package com.aihulk.tech.core.resource.entity;
+
+/**
+ * @author zhangyibo
+ * @title: ScoreTable
+ * @projectName aihulk
+ * @description: 评分卡
+ * @date 2019-07-29 14:19
+ */
+public class ScoreTable extends DecisionTable {
+
+}
